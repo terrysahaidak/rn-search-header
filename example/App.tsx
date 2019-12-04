16dce9087026c37c57a936c568895e892840c760
Terry Sahaidak <s_tasssik_taras@ukr.net>
2019-12-04 23:16:04 +0200

    Init

---
+++ example/App.tsx
@@ -0,0 +1,24 @@
+import React from 'react';
+import { createStackNavigator } from 'react-navigation-stack';
+import { createAppContainer } from 'react-navigation';
+import { HomeScreen } from './screens/HomeScreen/HomeScreen';
+
+export const routes = {};
+
+const StackNavigator = createStackNavigator(
+  {
+    Home: HomeScreen,
+    // ...routes,
+  },
+  {
+    initialRouteName: 'Home',
+  },
+);
+
+const Navigator = createAppContainer(StackNavigator);
+
+function App() {
+  return <Navigator />;
+}
+
+export default App;
